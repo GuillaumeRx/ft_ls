08324f9156a38956870b188d8b0376c2eb025cfd
Guillaume ROUX <guroux@e3r11p18.42.fr>
2019-03-12 17:16:38 +0100

    modified reverse sorting

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: guroux <guroux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 18:09:57 by guroux            #+#    #+#             */
-/*   Updated: 2019/02/19 19:17:01 by guroux           ###   ########.fr       */
+/*   Updated: 2019/03/12 15:29:57 by guroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,37 +19,22 @@ t_dir	*listswitch(t_dir *l1, t_dir *l2)
 	return (l2);
 }
 
-int		alpha(t_dir *node, t_opt *opt)
+int		alpha(t_dir *node)
 {
-	if (opt->rev == 1)
-		return (ft_strcmp(node->name, node->next->name) < 0 ? 1 : 0);
-	else
-		return (ft_strcmp(node->name, node->next->name) > 0 ? 1 : 0);
+	return (ft_strcmp(node->name, node->next->name) > 0 ? 1 : 0);
+}
+
+int		lastmod(t_dir *node)
+{
+	if (node->rawtime < node->next->rawtime)
+		return (1);
+	else if (node->rawtime == node->next->rawtime)
+		if (ft_strcmp(node->name, node->next->name) > 0)
+			return (1);
 	return (0);
 }
 
-int		lastmod(t_dir *node, t_opt *opt)
-{
-	if (opt->rev == 1)
-	{
-		if (node->rawtime > node->next->rawtime)
-			return (1);
-		else if (node->rawtime == node->next->rawtime)
-			if (ft_strcmp(node->name, node->next->name) < 0)
-				return (1);
-	}
-	else
-	{
-		if (node->rawtime < node->next->rawtime)
-			return (1);
-		else if (node->rawtime == node->next->rawtime)
-			if (ft_strcmp(node->name, node->next->name) > 0)
-				return (1);
-	}
-	return (0);
-}
-
-t_dir	*ft_sort(t_dir *start, int (*f)(t_dir *, t_opt *), t_opt *opt)
+t_dir	*ft_sort(t_dir *start, int (*f)(t_dir *))
 {
 	t_dir	*next;
 	t_dir	*act;
@@ -69,7 +54,7 @@ t_dir	*ft_sort(t_dir *start, int (*f)(t_dir *, t_opt *), t_opt *opt)
 			next = top->next;
 			while (next->next != NULL)
 			{
-				if (f(next, opt))
+				if (f(next))
 				{
 					act->next = listswitch(next, next->next);
 					changed = 1;
@@ -85,10 +70,31 @@ t_dir	*ft_sort(t_dir *start, int (*f)(t_dir *, t_opt *), t_opt *opt)
 	return (next);
 }
 
+void revlist(t_dir **start)
+{
+	t_dir	*prev;
+	t_dir	*curr;
+	t_dir	*next;
+
+	prev = NULL;
+	curr = *start;
+	next = NULL;
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next  = prev;
+		prev = curr;
+		curr = next;
+	}
+	*start = prev;
+}
+
 void	sortlist(t_dir **start, t_opt *opt)
 {
 	if (opt->tim == 1)
-		*start = ft_sort(*start, &lastmod, opt);
+		*start = ft_sort(*start, &lastmod);
 	else
-		*start = ft_sort(*start, &alpha, opt);
+		*start = ft_sort(*start, &alpha);
+	if (opt->rev == 1)
+		revlist(start);
 }
