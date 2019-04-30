dea07a55c0c1f8941a54625a87ca8d4996901407
Guillaume Roux <guillaumeroux123@gmail.com>
2019-04-30 11:51:43 +0200

    redirected error to stderr

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: guroux <guroux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 21:38:32 by guroux            #+#    #+#             */
-/*   Updated: 2019/04/25 22:13:26 by guroux           ###   ########.fr       */
+/*   Updated: 2019/04/30 11:50:45 by guroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,6 @@ void	usage(char opt)
 {
 	ft_putstr_fd("ft_ls: illegal option -- ", 2);
 	ft_putchar_fd(opt, 2);
-	ft_putchar('\n');
-	ft_putendl("usage: ft_ls [-Ralrt] [file ...]");
+	ft_putchar_fd('\n', 2);
+	ft_putendl_fd("usage: ft_ls [-Ralrt] [file ...]", 2);
 }
